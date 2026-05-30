d3d0a2dfd505e009cf025d3aa4850123f7ca4f5e
Ana Luisa <alurcosta08@gmail.com>
2026-05-30 14:28:02 -0300

    lógica dos filtros e busca de produtos

--- js/script.js
+++ js/script.js
@@ -40,3 +40,97 @@ function voltarSlide() {
 setInterval(() => {
     proximoSlide();
 }, 5000);
+
+// Filtros 
+
+const botoes = document.querySelectorAll(".filtro");
+const cards = document.querySelectorAll(".card-produto");
+
+botoes.forEach(botao => {
+
+  botao.addEventListener("click", () => {
+
+    const filtro = botao.dataset.filtro;
+
+    cards.forEach(card => {
+
+      const info = card.querySelector(".info-produto");
+
+      // mostra todos
+      if (filtro === "todos") {
+        card.style.display = "block";
+      }
+
+      // mostra os que tem a classe
+      else if (info.classList.contains(filtro)) {
+        card.style.display = "block";
+      }
+
+      // esconde o que não tem classe 
+      else {
+        card.style.display = "none";
+      }
+
+    });
+
+  });
+
+});
+
+// barra de pesquisa
+const busca = document.querySelector(".busca");
+
+
+busca.addEventListener("input", () => {
+
+  const texto = busca.value.toLowerCase();
+
+  cards.forEach(card => {
+
+    const nomeProduto = card
+      .querySelector("h3")
+      .textContent
+      .toLowerCase();
+
+    // verifica se o nome tem o texto digitado
+    if (nomeProduto.includes(texto)) {
+      card.style.display = "block";
+    } else {
+      card.style.display = "none";
+    }
+
+  });
+
+});
+
+// Filtros de preços
+
+const botoesPreco = document.querySelectorAll(".filtro-preco");
+
+botoesPreco.forEach(botao => {
+
+  botao.addEventListener("click", () => {
+
+    const preco = botao.dataset.preco;
+
+    cards.forEach(card => {
+
+      const info = card.querySelector(".info-produto");
+
+      if (preco === "todos") {
+        card.style.display = "block";
+      }
+
+      else if (info.classList.contains(preco)) {
+        card.style.display = "block";
+      }
+
+      else {
+        card.style.display = "none";
+      }
+
+    });
+
+  });
+
+});
